627fe1df986d81c833ee1d070b608bad70c81c25
Lauren A <laurenhaileyama@gmail.com>
2022-03-09 13:44:39 -0500

    update the index.js file with the initial add employee inquirer prompt

--- index.js
+++ index.js
@@ -4,3 +4,41 @@ const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { listenerCount } = require('process');
+
+
+const employees = [];
+
+function initApp() {
+    startHtml();
+    addEmployee();
+}
+
+function addEmployee() {
+    inquirer.prompt([
+        { 
+            type: "input",
+            message: "Enter Employee Name:",
+            name: "name"
+        },
+        {
+            type: "list",
+            message: "Select New Team Member's Role:",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Manager"
+            ],
+            name: "role"
+        },
+        {
+            type: "input",
+            message: "Please register Employee ID Number:",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "Enter Employee Email:",
+            name: "email",
+        }])
+}
